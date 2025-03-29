7aa2d04435f4ca26e4769c47c42a4b25fb430cb1
ChlorineFlow <priyanshubahuguna438@gmail.com>
2025-03-29 18:46:03 +0530

    LOGIN BUTTONS ADDED

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const path = require("path");
 
 app.use(express.static('public'));
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+
+// Serve static files (CSS, JS, images)app.use(express.static(path.join(__dirname, "public")));
 // Define a simple route
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.get('/about', (req, res) => {
-    res.render('about.ejs');
+app.get('/student_login', (req, res) => {
+    res.render('student_login.ejs');
 });
 
-app.get('/student', (req, res) => {
-    res.render('student.ejs');
+app.get('/faculty_login', (req, res) => {
+    res.render('faculty_login.ejs');
+});
+
+app.get('/admin_login', (req, res) => {
+    res.render('admin_login.ejs');
 });
 
 // Start the server
