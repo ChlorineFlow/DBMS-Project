c0d17afa7c59fd3bc8ac91008214bfac032569b8
ChlorineFlow <priyanshubahuguna438@gmail.com>
2025-03-29 17:21:44 +0530

    Routes Added

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about.ejs');
+});
+
+app.get('/student', (req, res) => {
+    res.render('student.ejs');
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
